22f53c7293fc374d45283df6da3071a4472b804b
Smol <nekit.smol@gmail.com>
2021-05-19 18:46:33 +0300

    Non-blocking client

--- Client/src/main.c
+++ Client/src/main.c
@@ -3,9 +3,10 @@
 #include <stdbool.h>
 #include <string.h>
 #include <netdb.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sys/poll.h>
 
 #include <request_encoder.h>
 #include <response_encoder.h>
@@ -14,14 +15,16 @@
 
 int main(int argc, char **argv)
 {
-    int socket_fd, reuse = 1, non_blocking = 1;
-    size_t len, request_len;
+    int socket_fd, len, status, timeout = 30 * 1000, socket_err;
+    socklen_t socket_err_size = sizeof(socket_err);
+    size_t request_len, command_len;
     char *colon_ptr, *host, *port, command_str[MAX_COMMAND_LEN], *space_ptr;
     unsigned char *request_buffer, response_buffer[RESPONSE_MAX_SIZE];
     bool quit = false;
     request req;
     response resp;
     struct addrinfo hints, *candidates, *candidate;
+    struct pollfd connection_fd;
 
     if (argc != 2 || !(colon_ptr = strchr(argv[1], ':'))) {
         printf("Usage: %s server_host:server_port", argv[0]);
@@ -36,7 +39,7 @@ int main(int argc, char **argv)
     hints.ai_socktype = SOCK_STREAM;
 
     if (getaddrinfo(host, port, &hints, &candidates)) {
-        printf("Failed to find suitable address");
+        printf("Failed to find suitable address\n");
         exit(EXIT_FAILURE);
     }
 
@@ -46,8 +49,8 @@ int main(int argc, char **argv)
             continue;
         }
 
-        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse))) {
-            printf("Failed to set socket options");
+        if (fcntl(socket_fd, F_SETFL, O_NONBLOCK)) {
+            printf("Failed to set a socket to be non-blocking\n");
             close(socket_fd);
             exit(EXIT_FAILURE);
         }
@@ -56,25 +59,36 @@ int main(int argc, char **argv)
     }
 
     if (connect(socket_fd, candidate->ai_addr, candidate->ai_addrlen)) {
-        printf("Failed to connect to host");
-        close(socket_fd);
-        exit(EXIT_FAILURE);
-    }
+        if (errno != EINPROGRESS) {
+            goto failed_to_connect;
+        }
 
-//    if (ioctl(socket_fd, FIONBIO, &non_blocking)) {
-//        printf("Failed to set a socket to be non-blocking");
-//        close(socket_fd);
-//        exit(EXIT_FAILURE);
-//    }
+        printf("Connecting to host...\n");
+        connection_fd.fd = socket_fd;
+        connection_fd.events = POLLOUT;
+
+        status = poll(&connection_fd, 1, timeout);
+        if (status <= 0) {
+            goto failed_to_connect;
+        }
+
+        if (getsockopt(socket_fd, SOL_SOCKET, SO_ERROR, &socket_err, &socket_err_size) || socket_err) {
+            failed_to_connect:
+            printf("Failed to connect to host\n");
+            close(socket_fd);
+            exit(EXIT_FAILURE);
+        }
+        printf("Successfully connected\n");
+    }
 
     freeaddrinfo(candidates);
 
     while (!quit) {
         printf(">> ");
         fgets(command_str, sizeof(command_str), stdin); //TODO: Move command parsing to a different source file
-        len = strlen(command_str);
-        if (command_str[len - 1] == '\n') {
-            command_str[len - 1] = 0;
+        command_len = strlen(command_str);
+        if (command_str[command_len - 1] == '\n') {
+            command_str[command_len - 1] = 0;
         }
 
         if ((space_ptr = strchr(command_str, ' '))) {
@@ -94,32 +108,36 @@ int main(int argc, char **argv)
 
             free(request_buffer);
 
-            len = recv(socket_fd, response_buffer, sizeof(response_buffer), 0);
-            if (len < 0) {
-                if (errno != EWOULDBLOCK) {
-                    printf("Failed to receive a response from server\n");
+            while (true) {
+                len = recv(socket_fd, response_buffer, sizeof(response_buffer), 0);
+                if (len < 0) {
+                    if (errno != EWOULDBLOCK) {
+                        printf("Failed to receive a response from server\nQuitting...\n");
+                        quit = true;
+                    }
+                    break;
                 }
-                break;
-            }
 
-            if (len == 0) {
-                printf("Server closed the connection\n");
-                quit = true;
-            }
+                if (len == 0) {
+                    printf("Server closed the connection\nQuitting...\n");
+                    quit = true;
+                    break;
+                }
 
-            decode_response(response_buffer, &resp);
-            switch (resp.header.code) {
-                case OP_OK: {
-                    printf("Success\n");
-                    break;
-                }
-                case OP_NOTFOUND: {
-                    printf("Server does not support such operation\n");
-                    break;
-                }
-                case OP_FAILED: {
-                    printf("Failed\n");
-                    break;
+                decode_response(response_buffer, &resp);
+                switch (resp.header.code) {
+                    case OP_OK: {
+                        printf("Success\n");
+                        break;
+                    }
+                    case OP_NOTFOUND: {
+                        printf("Server does not support such operation\n");
+                        break;
+                    }
+                    case OP_FAILED: {
+                        printf("Failed\n");
+                        break;
+                    }
                 }
             }
         } else if (!strcmp(command_str, "quit")) {
@@ -131,5 +149,6 @@ int main(int argc, char **argv)
         }
     }
 
+    close(socket_fd);
     exit(EXIT_SUCCESS);
 }
